3386c6429c2d8ec16a294d2fb49528d1cb09cdcc
José Manuel Domínguez E <josemanueldomingueze@gmail.com>
2022-10-21 14:10:37 -0700

    primer commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const elements = document.getElementsByClassName("width");
+while (elements.length > 0) elements[0].remove();
+
+
+const elementsFont = document.getElementsByClassName("class");
+while (elementsFont.length > 0) elementsFont[0].remove();
